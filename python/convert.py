5bd0caf3af9d7da3e3379e2c8c63c1f1f7de4be8
NamcheolLee <nclee@redwood.snu.ac.kr>
2025-10-03 15:24:05 +0900

    need to make convert.py into a flexible one

--- python/convert.py
+++ python/convert.py
@@ -2,15 +2,18 @@
 import torch
 from model import SimpleCNN
 from ai_edge_torch import convert
+import argparse
 
-# Dummy input for error checking during conversion
-example_input = (torch.randn(1, 1, 784),)
 
-model = SimpleCNN()
-state = torch.load("../models/simplecnn.pt", map_location=torch.device('cpu'))
-model.load_state_dict(state)
-output_path = "../models/simplecnn.tflite"
-edge_model = convert(model.eval(), example_input)
-edge_model.export(output_path)
+def main():
+    # Dummy input for error checking during conversion
+    example_input = (torch.randn(1, 1, 784),)
 
-print(f"TFLite model saved to: {output_path}")
+    model = SimpleCNN()
+    state = torch.load("../models/simplecnn.pt", map_location=torch.device('cpu'))
+    model.load_state_dict(state)
+    output_path = "../models/simplecnn.tflite"
+    edge_model = convert(model.eval(), example_input)
+    edge_model.export(output_path)
+
+    print(f"TFLite model saved to: {output_path}")

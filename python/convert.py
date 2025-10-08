3f62fa8c5bfc4382705869f1a3fbfdf52f4d37ce
zzilit11 <taehyunee517@naver.com>
2025-10-08 12:04:34 +0900

    modified convert.py - save model/tflite

--- python/convert.py
+++ python/convert.py
@@ -16,7 +16,7 @@ from onnx2tf import convert
 
 convert(
     input_onnx_file_path="./models/simple_classifier.onnx",
-    output_folder_path="./models",
+    output_folder_path="./models/tflite",
     copy_onnx_input_output_names_to_tflite=True,
     non_verbose=False,
 )

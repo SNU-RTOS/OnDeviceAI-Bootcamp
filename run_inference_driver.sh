1acfb80e0893e1022e19e550b05ffd4cd0ab26ee
zzilit11 <taehyunee517@naver.com>
2025-10-10 19:43:32 +0900

    10000 images

--- run_inference_driver.sh
+++ run_inference_driver.sh
@@ -21,7 +21,7 @@ gpu_usage="true"
 class_labels="class_labels.json"
 data_dir="./data/MNIST/test"    
 input_period_ms=0
-total_inputs=100                
+total_inputs=1000                
 # ---------------------------------
 
 # Sanity check for files and directories
